d9345551f69e960d7aa1fa809f239120a1a7e27c
Robert Little <rll04747@pomona.edu>
2021-05-18 18:08:13 -0700

    Add several tests for name option

--- src/SimpleCmdLine.Tests/UnitTest1.cs
+++ src/SimpleCmdLine.Tests/UnitTest1.cs
@@ -1,14 +1,33 @@
 using System;
 using Xunit;
+using SimpleCmdLine;
 
 namespace SimpleCmdLine.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void NoArgs_ReturnsFail()
         {
+            string[] args = new string[]{};
+            var result = Program.Main(args);
+            Assert.Equal(1, result);
+        }
 
+        [Fact]
+        public void OptionNoArgument_ReturnsFail()
+        {
+            string[] args = new string[]{ "--name" };
+            var result = Program.Main(args);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void OptionWithArgument_ReturnsFail()
+        {
+            string[] args = new string[]{ "--name", "Success"};
+            var result = Program.Main(args);
+            Assert.Equal(0, result);
         }
     }
 }
